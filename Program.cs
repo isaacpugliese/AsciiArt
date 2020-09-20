617fc0412968e524ee13e03ed13a948e495c89d7
Isaac Pugliese <isaacpugliese2010@gmail.com>
2020-09-20 13:12:20 -0300

    finalizado

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace asciiart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.WriteLine(" ||  ||");
+            Console.WriteLine("  \\()/");
+            Console.WriteLine(" /(__)\\");
+            Console.WriteLine("||    ||");
+            Console.ResetColor();
+        }
+    }
+}
